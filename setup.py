93ce4e746581d877b7584f265f6502eb17edbc63
okeyode22 <oiadebayo22@gmail.com>
2023-02-26 15:14:36 +0100

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="mysci",
+    version="1.0.0",
+    description="A sample package description",
+    author="Adebayo Okeyode",
+    author_email="okeyodeisiaqadebayo@gmail.com",
+    packages=["mysci"],
+    install_requires=[])
